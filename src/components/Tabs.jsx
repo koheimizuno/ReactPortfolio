e085d4bfbdb23a93cde3450f99e0f0a1a8616a35
skygentleman <skygentleman626@gmail.com>
2020-11-15 22:14:49 +0000

    Tab selectors (unfinished)

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,9 @@
 import { Component } from "react"
 import { AnimatePresence, motion } from "framer";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import { About } from "./tabs/About";
+import { Projects } from "./tabs/Projects";
+import { CV } from "./tabs/CV";
 
 import "./Tabs.scss";
 export class Tabs extends Component {
@@ -11,11 +13,17 @@ export class Tabs extends Component {
         this.state = {
             tab: 0,
         }
-        const tabCount = 1;
-        //About
+        const tabCount = 3;
+        //About = 0
+        //Projects = 1
+        //CV = 2
+
+        this.switchTab = this.switchTab.bind(this);
     }
 
-
+    switchTab(i) {
+        this.setState({ tab: i });
+    }
 
     render() {
         let initialCss = {
@@ -28,14 +36,37 @@ export class Tabs extends Component {
         let tab;
         switch (this.state.tab) {
             case 0: tab = <About />; break;
+            case 1: tab = <Projects />; break;
+            case 2: tab = <CV />; break;
         }
 
         return this.props.visible &&
             (
                 <AnimatePresence>
                     <motion.div className="tabs" initial={initialCss} animate={animateCss}>
-                        <Container className="h-100 dark">
-                            {tab}
+                        <Container className="h-100">
+                            <Row className="mx-0">
+                                <Col xs={4}
+                                    className={`tabSelector ${this.state.tab == 0 ? "--active" : ""}`}
+                                    onClick={() => { this.switchTab(0) }}>
+                                    About
+                                </Col>
+                                <Col xs={4}
+                                    className={`tabSelector ${this.state.tab == 1 ? "--active" : ""}`}
+                                    onClick={() => { this.switchTab(1) }}>
+                                    Projects
+                                </Col>
+                                <Col xs={4}
+                                    className={`tabSelector ${this.state.tab == 2 ? "--active" : ""}`}
+                                    onClick={() => { this.switchTab(2) }}>
+                                    CV
+                                </Col>
+                            </Row>
+                            <div className="dark h-100 overflow-auto pt-3">
+                                <AnimatePresence>
+                                    {tab}
+                                </AnimatePresence>
+                            </div>
                         </Container>
                     </motion.div>
                 </AnimatePresence>
--- src/components/tabs/About.jsx
+++ src/components/tabs/About.jsx
@@ -1,8 +1,47 @@
+import { motion } from 'framer';
+import { Container, Row, Col } from 'reactstrap';
 
 export function About(props) {
+
+    let initialCss = {
+        opacity: 0
+    }
+    let animateCss = {
+        opacity: 1
+    }
+    let exitCss = {
+        opacity: 0
+    }
+
     return (
-        <div>
-            <h1>About</h1>
-        </div>
+        <motion.div
+            initial={initialCss}
+            animate={animateCss}
+            exit={exitCss}>
+            <Container>
+                <p>
+                    Programmer and web developer, I am always looking to learn.
+                    Primarily working with .NET and React, I enjoy creating both standalone applications, web services and APIs, and games.
+                </p>
+                <h2 className="pt-3">Want to contact me?</h2>
+                <p>Get in touch for any reason: about a project, more information about me, or just to say hi!</p>
+                <Row>
+                    <Col className="text-center" xs="1">
+                    <i className="fas fa-envelope"></i>
+                    </Col>
+                    <Col xs="auto">
+                        <a className="hover" href="mailto:oliver.beckwith1@gmail.com">oliver.beckwith1@gmail.com</a>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="text-center" xs="1">
+                        <i className="fab fa-linkedin"></i>
+                    </Col>
+                    <Col xs="auto">
+                        <a className="hover" href="https://linkedin.com/in/OliverBeckwith">OliverBeckwith</a>
+                    </Col>
+                </Row>
+            </Container>
+        </motion.div>
     );
 }
