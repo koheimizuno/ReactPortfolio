e50e42d28db2042d40fdf3927a104a8a2d13e43d
skygentleman <skygentleman626@gmail.com>
2020-11-29 17:34:58 +0000

    Skills display and show tooltips

--- src/components/tabs/Skills.jsx
+++ src/components/tabs/Skills.jsx
@@ -1,23 +1,72 @@
+import { render } from '@testing-library/react';
 import { motion } from 'framer';
+import { Component } from 'react';
+import { Row, Col } from 'reactstrap';
 
-export function Skills(props) {
-    
-    let initialCss = {
-        opacity: 0
-    }
-    let animateCss = {
-        opacity: 1
-    }
-    let exitCss = {
-        opacity: 0
+import './Skills.scss';
+
+export class Skills extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            skills: undefined
+        }
     }
 
-    return (
-        <motion.div
-            initial={initialCss}
-            animate={animateCss}
-            exit={exitCss}>
-            <h1>Skills</h1>
-        </motion.div>
-    );
+    componentDidMount() {
+        this.loadSkills();
+    }
+
+    async loadSkills() {
+        const response = await fetch("skills.json");
+        let skills;
+        try{
+            skills = await response.json();
+        }
+        catch(e){}
+        this.setState({ skills: skills });
+    }
+
+    render() {
+        let initialCss = {
+            opacity: 0
+        }
+        let animateCss = {
+            opacity: 1
+        }
+        let exitCss = {
+            opacity: 0
+        }
+
+        let skills;
+
+        if (this.state.skills===null || (this.state.skills && this.state.skills.length == 0))
+            skills = <Col>Error Loading Skills</Col>;
+        else if (this.state.skills)
+            skills = this.state.skills.map((cat) => 
+                {
+                    return <div className="skill-category">
+                        <h1>{cat.category}</h1>
+                        <Row>
+                            {cat.skills.map((skill) => {
+                                return <Col className="skill-box">
+                                        <div className="skill-tooltip">{skill.ability}</div>
+                                        <p className="skill-text">{skill.name}</p>
+                                    </Col>
+                            })}
+                        </Row>
+                    </div>;
+                });
+
+        return (
+            <motion.div
+                initial={initialCss}
+                animate={animateCss}
+                exit={exitCss}
+                className="container">
+                {skills}
+            </motion.div>
+        );
+    }
 }
